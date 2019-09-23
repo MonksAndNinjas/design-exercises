23a3190b7efb5f86b55e6fa9fa48f4a5f6384816
blackbeltdiaries <github email address>
2019-09-23 14:19:22 -0600

    created circleColorSpectrum() in helloColors.js

--- empty-example/helloColors.js
+++ empty-example/helloColors.js
@@ -23,7 +23,7 @@ function interactiveColorBoxes() {
   */
 }
 
-function colorSpectrum() {
+function rectangleColorSpectrum() {
   /*  global variables:
       var stepX;
       var stepY;
@@ -48,5 +48,60 @@ function colorSpectrum() {
   }
 
   // Mouse: Position x/y: Grid resolution
-  // Keys: S: Saveimage
+}
+
+function circleColorSpectrum() {
+  /* global variables:
+     var segmentCount = 360;
+     var radius = 300;
+
+     setup() parameters:
+     createCanvas(800, 800);
+     noStroke();
+  */
+
+  colorMode(HSB, 360, width, height);
+  background(360, 0, height);
+
+  // angle increment depends on how many segments are to be drawn
+  var angleStep = 360 / segmentCount;
+
+  beginShape(TRIANGLE_FAN);
+  vertex(width / 2, height / 2);  // placed in the middle of the canvas
+
+  for (var angle = 0; angle <= 360; angle += angleStep) {
+    // convert angle from degrees to radians
+    var vx = width / 2 + cos(radians(angle)) * radius;
+    var vy = height / 2 + sin(radians(angle)) * radius;
+    // fill color for next segment defined below in fill()
+    vertex(vx, vy);
+    fill(angle, mouseX, mouseY);
+  }
+  // end construction of shape
+  endShape();
+
+  /* Add to keyPressed() changes size of circle and it's color values
+
+  switch (key) {
+    case '1':
+      segmentCount = 360;
+      break;
+    case '2':
+      segmentCount = 45;
+      break;
+    case '3':
+      segmentCount = 24;
+      break;
+    case '4':
+      segmentCount = 12;
+      break;
+    case '5':
+      segmentCount = 6;
+      break;
+  }
+
+  Mouse: Position x: Saturation
+        Position y: Brightness
+
+  Keys: 1-5: Segment Count */
 }
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -3,20 +3,35 @@ console.log(
   "Now rendering - " +
   //"followMouse() => draws circles that follow mouse movement"
   //"interactiveColorBoxes() => boxes with colors in it"
-  "colorSpectrum() => displays color range in a rectangle"
+  //"rectangleColorSpectrum() => displays color range in a rectangle"
+  //"circleColorSpectrum() => displays color range in a circle"
+  "colorPalletes() => create color groups in every gradiation"
 );
 
-var stepX;
-var stepY;
+//global variables
+var tileCountX = 2;
+var tileCountY = 10;
+
+var colorsLeft = [];
+var colorsRight = [];
+var colors = [];
+
+var interpolateShortest = true;
 
 // variables to add to the canvas before drawing begins
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(800, 800);
+  colorMode(HSB);
   noStroke();
-  colorMode(HSB, width, height, 100);
+  shakeColors();
 }
 
 // repeats draw function
 function draw() {
-  colorSpectrum();
+  colorPalletes();
+}
+
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png'); // saves image12
 }
