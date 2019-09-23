8f222a72d2c8bda5e8c8955766d4872a5254f6a8
blackbeltdiaries <github email address>
2019-09-23 13:36:19 -0600

    added interactiveColorBoxes() to helloColors.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,12 +1,21 @@
+// Add the following functions to see what they do:
+console.log(
+  "Now rendering - " +
+  //"followMouse() => draws circles that follow mouse movement"
+  "interactiveColorBoxes() => boxes with colors in it"
+);
 
-
+// variables to add to the canvas before drawing begins
 function setup() {
   createCanvas(720, 720);
-  noFill();
-  console.log('Hallo');
+  noCursor();
+
+  colorMode(HSB, 360, 100, 100);
+  rectMode(CENTER);
+  noStroke();
 }
 
+// repeats draw function
 function draw() {
-  // put drawing code here
-  followMouse()
+  interactiveColorBoxes();
 }
